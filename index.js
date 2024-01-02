9c4da9ced515b99f17bc4fcc0d0d88d1df44aa80
HackItSapiens <155453891+HackItSapiens@users.noreply.github.com>
2024-01-02 19:44:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+console.log("helloo")
+
+let lt = document.getElementById('lt');
+let lm = document.getElementById('lm');
+let dt = document.getElementById('dt');
+let dm = document.getElementById('dm');
+let dd = document.getElementById('dd');
+let ddt = document.getElementById('ddt');
+let lg = document.getElementById('logo');
+console.log("lt style",lt)
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    lt.style.top = value * 0.25 + 'px';
+    dd.style.top = value * 1.25 + 'px';
+    lg.stylle.top = value*1.25 +'px';
+})
+
+function footMouseFollower(){
+    window.addEventListener('mousemove', function(){
+        this.document.querySelector('#fim').style.transform = `translate(${dets.clientX}px , ${dets.clientY}px)`;
+    })
+}
